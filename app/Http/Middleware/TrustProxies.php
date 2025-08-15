630c07fc1a473b1dccd398b3849397c023bb1649
swadmahfuz <swad.mahfuz@gmail.com>
2025-08-16 02:20:46 +0600

    Update TrustProxies.php

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -13,13 +13,3 @@ class TrustProxies extends Middleware
     // If you're behind Cloudflare, you can also use:
     // protected $headers = Request::HEADER_X_FORWARDED_PROTO | Request::HEADER_X_FORWARDED_HOST | Request::HEADER_X_FORWARDED_PORT | Request::HEADER_X_FORWARDED_FOR;
 }
-/*
- |--------------------------------------------------------------------------
- | Trust Proxies Middleware
- |--------------------------------------------------------------------------
- |
- | This middleware is responsible for trusting proxies and handling
- | the headers that are used to determine the client's IP address.
- |
- | Developed by: Swad Ahmed Mahfuz (Head of Divison - Business Assurance & Training, Bangladesh)
- | Contact:
